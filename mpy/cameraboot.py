2590627c06492629de76c4226c9d0f9463ddb275
yurashkurenko <yurashkurenko@users.noreply.github.com>
2024-05-05 13:41:38 +0300

    Update cameraboot.py

--- mpy/cameraboot.py
+++ mpy/cameraboot.py
@@ -1,14 +1,11 @@
-import uos
-import machine
-import time
-
-led = machine.Pin(4, machine.Pin.OUT)
+#led = machine.Pin(4, machine.Pin.OUT)
 import camera
-
-led.on()
-time.sleep(1)
 camera.init(0, format=camera.JPEG, fb_location=camera.DRAM)
-time.sleep(1)
-buf=camera.capture()
-camera.deinit()
-led.off()
+
+#led.on()
+#time.sleep(1)
+#camera.init(0, format=camera.JPEG, fb_location=camera.DRAM)
+#time.sleep(1)
+#buf=camera.capture()
+#camera.deinit()
+#led.off()
