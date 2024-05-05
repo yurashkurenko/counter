1e00ab466a22f375df7a19a9ecfed32519f58095
yurashkurenko <yurashkurenko@users.noreply.github.com>
2024-05-05 11:36:43 +0300

    Update cameraboot.py

--- mpy/cameraboot.py
+++ mpy/cameraboot.py
@@ -1,4 +1,10 @@
 import uos
 import machine
+import time
 led = machine.Pin(4, machine.Pin.OUT)
-#uos.mount(machine.SDCard(), "/sd")
+import camera
+led.on()
+camera.init(0, format=camera.JPEG, fb_location=camera.DRAM)
+buf=camera.capture()
+camera.deinit()
+led.off()
