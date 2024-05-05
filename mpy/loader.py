adc35a685f1b02d5274e4687cedd4c05a272b517
yurashkurenko <yurashkurenko@users.noreply.github.com>
2024-05-05 12:37:56 +0300

    Update loader.py

--- mpy/loader.py
+++ mpy/loader.py
@@ -1,1 +1,11 @@
 import nw-one
+import requests
+#url = "https://raw.githubusercontent.com/yurashkurenko/counter/main/hello.py"
+#progname="helloprog.py"
+from paraprog import url, progname
+r=requests.get(url)
+print(r.text)
+with open(progname, "w") as f:
+    # Write the text data to the file
+    f.write(r.text)
+import progname
