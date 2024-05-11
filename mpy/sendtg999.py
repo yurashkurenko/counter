4eecc9daf4ad047ad254b5d1e9c822b1cce131a8
yurashkurenko <yurashkurenko@users.noreply.github.com>
2024-05-11 07:47:20 +0300

    Create sendtg999.py

---
+++ mpy/sendtg999.py
@@ -0,0 +1,9 @@
+    def send(self, chat_id, text):
+        data = {'chat_id': chat_id, 'text': text}
+        try:
+            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+            response = urequests.post(self.url + '/sendMessage', json=data, headers=headers)
+            response.close()
+            return True
+        except:
+            return False
