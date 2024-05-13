25da7b211da3943ba82e770c0938f0c90a9ec37f
yurashkurenko <yurashkurenko@users.noreply.github.com>
2024-05-13 20:19:43 +0300

    Update mpy

---
+++ mpy/sftg1.py
@@ -0,0 +1,23 @@
++sftg1.py
+ubinascii
+uos
+urequests
+random
+="--97d81ac404017fec19458e34bae65b01--"
+\\r\\n\\r\\n"
+="Content-Disposition: form-data; name="chat_id"\\r\\n\\r\\n159085018\\r\\n"
+""
+"
+undary+crlf
+ta)
+="--97d81ac404017fec19458e34bae65b01--"
+\r\\n\' send: b\'--97d81ac404017fec19458e34bae65b01\\r\\nContent-Disposition: form-data;                  at_id"\\r\\n\\r\\n159085018\\r\\n--97d81ac404017fec19458e34bae65b01\\r\\n
+Disposition: form-data; name="photo"; filename="Magic_Poser.jpg"
+iformdata(chatid,image):
+dary="--97d81ac404017fec19458e34bae65b01--"
+ = "\\r\\n\\r\\n\"
+ids =f"Content-Disposition: form-data; name={chatid}chat_id
+s = ""
+ = ""
++=boundary+crlf
+rn data
