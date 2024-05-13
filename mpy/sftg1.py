e524e3a307c5f44239a351ee5aa2d6dc0fcad47c
yurashkurenko <yurashkurenko@users.noreply.github.com>
2024-05-14 00:00:40 +0300

    Update sftg1.py

--- mpy/sftg1.py
+++ mpy/sftg1.py
@@ -1,9 +1,10 @@
-boundary="--97d81ac404017fec19458e34bae65b01--"
-crlf = "\\r\\n\\r\\n"
-chatpart="Content-Disposition: form-data; name="chat_id"\r\n\r\n159085018\r\n"
-imagepart="Disposition: form-data; name=\"photo\"; filename=\"Magic_Poser.jpg"
-def multupartdata(chatid,image):
-    mpdata=boundary+crlf+chatpart+boundary+crlf+imagepart+boundary+crlf+boundary
+def multupartdata(chat_id,image):
+    boundary="--97d81ac404017fec19458e34bae65b01--"
+    crlf = "\\r\\n\\r\\n"
+    chatpart="Content-Disposition: form-data; name="+chat_id+"\r\n\r\n159085018\r\n"
+    filename="Magic_Poser.jpg"
+    imagepart="Disposition: form-data; name="+photo+"; filename="+filename
+    mpdata=boundary+crlf+chatpart+boundary+crlf+imagepart+crlf+boundary
     print(mpdata)
     return mpdata
      
