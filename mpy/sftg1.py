78f47bead75d84c8225d7abb7a329d11ee0e3554
yurashkurenko <yurashkurenko@users.noreply.github.com>
2024-05-13 21:43:20 +0300

    Update sftg1.py

--- mpy/sftg1.py
+++ mpy/sftg1.py
@@ -1,23 +1,7 @@
-+sftg1.py
-ubinascii
-uos
-urequests
-random
-="--97d81ac404017fec19458e34bae65b01--"
-\\r\\n\\r\\n"
-="Content-Disposition: form-data; name="chat_id"\\r\\n\\r\\n159085018\\r\\n"
-""
-"
-undary+crlf
-ta)
-="--97d81ac404017fec19458e34bae65b01--"
-\r\\n\' send: b\'--97d81ac404017fec19458e34bae65b01\\r\\nContent-Disposition: form-data;                  at_id"\\r\\n\\r\\n159085018\\r\\n--97d81ac404017fec19458e34bae65b01\\r\\n
-Disposition: form-data; name="photo"; filename="Magic_Poser.jpg"
-iformdata(chatid,image):
-dary="--97d81ac404017fec19458e34bae65b01--"
- = "\\r\\n\\r\\n\"
-ids =f"Content-Disposition: form-data; name={chatid}chat_id
-s = ""
- = ""
-+=boundary+crlf
-rn data
+boundary="--97d81ac404017fec19458e34bae65b01--"
+crlf = "\\r\\n\\r\\n\"
+chatpart="Content-Disposition: form-data; name="chat_id"\r\n\r\n159085018\r\n"
+imagepart="Disposition: form-data; name="photo"; filename="Magic_Poser.jpg"
+def multupartdata(chatid,image):
+    return 
+     
