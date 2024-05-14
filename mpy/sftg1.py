61cc880bf2619fa4cf6784d9948e37e96e5eb41f
yurashkurenko <yurashkurenko@users.noreply.github.com>
2024-05-14 06:50:07 +0300

    Update sftg1.py

--- mpy/sftg1.py
+++ mpy/sftg1.py
@@ -12,7 +12,10 @@ def multupartdata(chat_id,image):
     
 def sendphotototg(token,chat_id,image):
     url="api.telegram.org"
-    header="Content-Type: multipart/form-data"
+    urltile="/bot1701291551:AAEk2TbH7mbHLPVVBrP0gEOSZBezbjH1FHY/sendPhoto HTTP/1.1\r\n"
+    #User-Agent: python-requests/2.31.0\r\nAccept-Encoding: gzip, deflate\r\n
+    #Content-Type: multipart/form-data; boundary=97d81ac404017fec19458e34bae65b01\r\n\r\n'
+    header="Content-Type: multipart/form-data; boundary=97d81ac404017fec19458e34bae65b01\r\n\r\n"
     data=multupartdata(chat_id,image)
     print(url)
     print(url)
