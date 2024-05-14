313f2f2c25ecb9ff83736ff1c63fbead71ffd018
yurashkurenko <yurashkurenko@users.noreply.github.com>
2024-05-14 06:59:51 +0300

    Update upgit.py

--- upgit.py
+++ upgit.py
@@ -1,4 +1,4 @@
 import os
 #os.system("cp upgit.py /")
 os.system("rm -r /counter2")
-os.system("git clone https://github.com/yurasshkurenko/counter /counter2")
+os.system("git clone https://github.com/yurashkurenko/counter /counter2")
