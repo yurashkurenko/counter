51726bfc000c4b2495180f1321253fa49ffb78cd
yurashkurenko <yurashkurenko@users.noreply.github.com>
2024-05-15 12:47:44 +0300

    Update sendphototg1.py

--- sendphototg1.py
+++ sendphototg1.py
@@ -7,7 +7,9 @@ def send_photo(chat_id, photo_path, token):
   url = f'https://api.telegram.org/bot{token}/sendPhoto'                              
   with open(photo_path, 'rb') as photo:         
     files = {'photo': photo}                  
-    data = {'chat_id': chat_id}               
+#    data = {'chat_id': chat_id}
+#    data = {'chat_id': chat_id,'img': base64.b64encode(image_data).decode('ascii'),'caption': 'картинка в телеграме'}
+    data = {'chat_id': chat_id,'caption': 'картинка в телеграме'}
     r = requests.post(url, files=files, data=data)                                      
   return r.json()                                                             
 # Использование                           
