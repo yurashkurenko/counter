b4f7ee619214f81622df6d2e042089919df0baa9
yurashkurenko <yurashkurenko@users.noreply.github.com>
2024-05-21 09:50:36 +0300

    Update

---
+++ iddevdb.py
@@ -0,0 +1,19 @@
+from vedis import Vedis
+IDATABASE =  'idevices.vdb'
+
+def getdevparafromid(dev_id):
+    with Vedis(IDATABASE) as idb:
+        para=idb[dev_id].decode()
+        return para
+
+def getdevparajsonfromid(dev_id):
+    with Vedis(IDATABASE) as idb:
+        para=idb[dev_id].decode()
+        id_dev=para.split(",")[0]
+        wifissid=para.split(",")[1]
+        description=para.split(",")[2]
+        polling=para.split(",")[3]
+        chatid=para.split(",")[4]
+        return {"id_dev":id_dev,"wifissid":wifissid, \
+        "description":description, "polling":polling, \
+        "chatid":chatid}
