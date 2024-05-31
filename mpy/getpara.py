45df103ed8080a6213fe3785e16b6e9589db4826
yurashkurenko <yurashkurenko@users.noreply.github.com>
2024-05-31 16:37:42 +0300

    Update getpara.py

--- mpy/getpara.py
+++ mpy/getpara.py
@@ -1,27 +1,10 @@
 import urequests as requests
 
-# Указываем URL сервера, к которому обращаемся
-url = 'https://tgdj.ubnt.cloudns.cl/devpara'
-
-# Задаём device_id, который хотим запросить
-device_id = '1212121212'
-
-# Создаём словарь с данными, которые будут отправлены в POST-запросе
-data = { 'device_id': device_id }
-
-# Дополнительно можно задать заголовки, если API требует их наличие
-headers = { 'Content-Type': 'application/json' }
-#    ,
-#    'Authorization': 'Bearer your_token_here'  # Пример токена, если нужна авторизация
-#}
-
-# Отправляем POST-запрос
-response = requests.post(url, json=data, headers=headers)
-
-# Пров&еряем статус ответа
-if response.status_code == 200:
-    # Выводим результат запроса, если ответ успешный
-    print("Response from server:", response.json())
-else:
-    # Выводим сообщение об ошибке, если запрос не прошёл
-    print("Failed to get device parameters. Status code:", response.status_code)
+def getpara(url,dev)
+    data = { 'device_id': dev }
+    headers = { 'Content-Type': 'application/json' }
+    response = requests.post(url, json=data, headers=headers)
+    if response.status_code == 200:
+        print("Response from server:", response.json())
+    else:
+        print("Failed to get device parameters. Status code:", response.status_code)
