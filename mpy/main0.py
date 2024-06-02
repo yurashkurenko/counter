bb7c17b3e3f1665b54ea78679265ecef3b282dca
yurashkurenko <yurashkurenko@users.noreply.github.com>
2024-06-02 21:26:45 +0300

    Update main0.py

--- mpy/main0.py
+++ mpy/main0.py
@@ -2,3 +2,12 @@ from settings import url,deviceid
 from getpara import getpara
 para=getpara(url,deviceid)
 print(para)
+from settings import url,deviceid
+from getpara import getpara
+para=getpara(url,deviceid)
+CAPTION=para['description']
+IMAGE_FILENAME='girl.jpg'
+#TOKEN, CHATID, IMAGE_FILENAME, CAPTION
+print(CAPTION)
+import sendtg6
+sendtg6.send_photo(TOKEN, CHATID, IMAGE_FILENAME, CAPTION)
