5a5587aa90254cb49fa7d6d1b137faf9f57ac145
yurashkurenko <yurashkurenko@users.noreply.github.com>
2024-06-25 10:59:55 +0300

    Update iddevdb.py

--- iddevdb.py
+++ iddevdb.py
@@ -11,8 +11,10 @@ def getdevparajsonfromid(dev_id):
         para=idb[dev_id].decode()
         id_dev=para.split(",")[0]
         wifissid=para.split(",")[1]
+        wifipwd=para.split(",")[1]
         description=para.split(",")[2]
         polling=para.split(",")[3]
+        lighting=para.split(",")[1]
         chatid=para.split(",")[4]
         return {"id_dev":id_dev,"wifissid":wifissid, \
         "description":description, "polling":polling, \
